c727389c74b36ebb970d444666c6a293101a36d5
Joshua Kisb <joshuakisb@gmail.com>
2023-10-04 13:50:35 +0300

    add empty dashboard layout

---
+++ pages/dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Dashboard</h1>
+</template>
+<script setup lang="ts">
+
+const name = "dashboard";
+
+const components = {  };
+
+definePageMeta({
+  layout: "dashboard",
+});
+
+</script>
