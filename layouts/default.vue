7d086e2433a8fe3d71c7399b7fba0ca5e30a5e63
aronmakiika <aronmakiika@gmail.com>
2023-10-09 23:27:23 +0300

    chore: add navbar and footer in auth and default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,17 @@
 <template>
-  <div>
-    <slot />
-  </div>
-  <!-- <core-app>
-        <template v-slot:default="{ isAuth, home, direction }">
-            <div id="app"
-                :dir="direction">
-                <document-title/>
-                <loader/>
-                <fade>
-                    <auth v-if="!isAuth"/>
-                    <home v-else-if="home"/>
-                    <default v-else/>
-                </fade>
-            </div>
-        </template>
-    </core-app> -->
+  <v-app>
+    <!-- <loader /> -->
+    <!-- <document-title /> -->
+    <fade>
+      <NavBar />
+      <slot />
+      <Footer />
+    </fade>
+  </v-app>
 </template>
 
 <script>
-// import { Fade } from "@liberu-ui/transitions";
-// import DocumentTitle from "@liberu-ui/ui/src/core/components/DocumentTitle.vue";
-// import Loader from "@liberu-ui/ui/src/bulma/components/Loader.vue";
-// import Auth from "@liberu-ui/auth/src/bulma/layouts/Auth.vue";
-// import Home from "@liberu-ui/ui/src/bulma/layouts/Home.vue";
-// import Default from "@liberu-ui/ui/src/bulma/layouts/Default.vue";
-
-// export default {
-//   name: "Root",
-
-//   components: {
-//     DocumentTitle,
-//     Loader,
-//     Fade,
-//     Auth,
-//     Home,
-//     Default,
-//   },
-// };
+import { Fade } from "@liberu-ui/transitions";
+//import DocumentTitle from "@liberu-ui/ui/src/core/components/DocumentTitle.vue";
+//import Loader from "@liberu-ui/ui/src/bulma/components/Loader.vue";
 </script>
