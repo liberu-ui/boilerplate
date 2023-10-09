4ed587e60a994dee16c8bdb776201d7296232746
aronmakiika <aronmakiika@gmail.com>
2023-10-10 00:26:27 +0300

    refactor: login page to use vue composition api

--- pages/login.vue
+++ pages/login.vue
@@ -1,63 +1,16 @@
 <template>
-  <login-form action="Login" route="login" @success="init" />
+  <auth-login-form action="Login" route="login" @success="init" />
 </template>
-<script>
+<script setup lang="ts">
+const payload = reactive({
+  email: "",
+  password: "",
+  remember: false,
+});
 
-import { mapActions, mapState } from "pinia";
-import Errors from "@liberu-ui/laravel-validation";
-import { useMaintStore, useAuthStore, useLayoutStore } from "~/store";
+const layout = "auth";
 
-import LoginForm from '~/components/auth/LoginForm.vue';
-
-export default {
- 
-  meta: {
-    guestGuard: true,
-    title: "Login",
-  },
-
-  components: { LoginForm },
-  setup() {
-
-    definePageMeta({
-      layout: "index",
-    });
-
-  },
-  data() {
-    return {
-      errors: new Errors(),
-      payload: {
-        email: "",
-        password: "",
-        remember: false,
-      },
-    };
-  },
-
-  computed: {
-    ...mapState(useMaintStore, ["meta"]),
-  },
-  methods: {
-    ...mapActions(useMaintStore, ["setShowQuote", "setCsrfToken"]),
-    ...mapActions(useAuthStore, ["login"]),
-    ...mapActions(useLayoutStore, ["home"]),
-    init(data) {
-      this.setShowQuote(this.meta.showQuote);
-      if (data?.csrfToken) {
-        this.setCsrfToken({ token: data.csrfToken, $axios: this.$axios });
-      }
-
-      setTimeout(() => {
-        if (data.role_id == 4) {
-          this.$router.push("/subscription");
-        } else {
-          this.$router.push("/dashboard");
-        }
-        this.login();
-        this.home(true);
-      }, 500);
-    },
-  },
-};
+function init() {
+  // TO DO implement init function
+}
 </script>
